6afefeff1e4b50404fc2e2eed12605f0cf853c57
kotarosz727 <kotarosz727@gmail.com>
2023-01-06 11:58:51 +0900

    revise

--- tests/tdd/CyclicNumberRuleTest.php
+++ tests/tdd/CyclicNumberRuleTest.php
@@ -10,14 +10,14 @@ class CyclicNumberRuleTest extends TestCase
     {
         $class = new CyclicNumberRule(3, 'Fizz');
 
-        $this->assertEquals('Fizz', $class->replace(3));
-        $this->assertEquals('Fizz', $class->replace(6));
-        $this->assertEquals('7', $class->replace(7));
+        $this->assertEquals('Fizz', $class->replace(3, ''));
+        $this->assertEquals('Fizz', $class->replace(6, ''));
+        $this->assertEquals('', $class->replace(7, ''));
 
         $class = new CyclicNumberRule(5, 'Buzz');
 
-        $this->assertEquals('Buzz', $class->replace(5));
-        $this->assertEquals('Buzz', $class->replace(10));
-        $this->assertEquals('7', $class->replace(7));
+        $this->assertEquals('Buzz', $class->replace(5, ''));
+        $this->assertEquals('Buzz', $class->replace(10, ''));
+        $this->assertEquals('', $class->replace(7, ''));
     }
 }
