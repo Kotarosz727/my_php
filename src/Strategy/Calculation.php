84dd6e12050bb9fffc08b46841cc7ec9e1205152
kotarosz727 <kotarosz727@gmail.com>
2023-01-11 08:13:35 +0900

    strategy patttern

--- src/Strategy/Calculation.php
+++ src/Strategy/Calculation.php
@@ -10,3 +10,9 @@ class Calculation
         return $this->strategy->calculate($vars);
     }
 }
+
+$plus = new Calculation(new PlusStrategy());
+$plus->execute([1, 1]);
+
+$minus = new Calculation(new MinusStrategy());
+$minus->execute([3, 1]);
